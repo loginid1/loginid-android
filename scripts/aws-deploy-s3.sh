431a8be4380bb6053e1b764e906a409d37034bea
braagaa <michael_braga@outlook.com>
2026-07-08 01:16:18 -0400

    add deploy pipeline

---
+++ scripts/aws-deploy-s3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "Obtaining version from tag..."
+echo "Module: $MODULE"
+echo "Version: $VERSION"
+
+if [ -f "$MODULE/build/outputs/aar/$MODULE-release.aar" ]; then
+    FILE_PATH="$MODULE/build/outputs/aar/$MODULE-release.aar"
+elif [ -f "$MODULE/build/libs/$MODULE.jar" ]; then
+    FILE_PATH="$MODULE/build/libs/$MODULE.jar"
+else
+    echo "No artifact found"
+    exit 1
+fi
+
+LATEST_PATH="loginid-android/$MODULE/latest/$MODULE-release.aar"
+S3_VERSIONED_PATH="loginid-android/$MODULE/$VERSION/$MODULE-release.aar"
+
+echo "Uploading $FILE_PATH to s3://$S3_BUCKET/$LATEST_PATH"
+aws s3 cp "$FILE_PATH" "s3://$S3_BUCKET/$LATEST_PATH"
+
+echo "Uploading $FILE_PATH to s3://$S3_BUCKET/$S3_VERSIONED_PATH"
+aws s3 cp "$FILE_PATH" "s3://$S3_BUCKET/$S3_VERSIONED_PATH"
