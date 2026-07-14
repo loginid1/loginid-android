0809433b0b28465080c84545117da883b8371c18
braagaa <michael_braga@outlook.com>
2026-07-14 03:17:18 -0400

    refactor: unify SDK versioning and streamline build/deploy process

--- scripts/aws-deploy-s3.sh
+++ scripts/aws-deploy-s3.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 set -e
 
-MODULE="${CI_COMMIT_TAG%%-*}"
-VERSION="${CI_COMMIT_TAG#*-}"
+VERSION="${CI_COMMIT_TAG}"
 
 echo "Obtaining version from tag..."
-echo "Module: $MODULE"
 echo "Version: $VERSION"
 
-if [ -f "$MODULE/build/outputs/aar/$MODULE-release.aar" ]; then
-    FILE_PATH="$MODULE/build/outputs/aar/$MODULE-release.aar"
-elif [ -f "$MODULE/build/libs/$MODULE.jar" ]; then
-    FILE_PATH="$MODULE/build/libs/$MODULE.jar"
-else
-    echo "No artifact found"
+upload_sdk() {
+    local sdk_name=$1
+    local sdk_path="build/loginid-${sdk_name}-sdk.aar"
+
+    if [ ! -f "$sdk_path" ]; then
+        echo "$sdk_path not found"
+        exit 1
+    fi
+
+    echo "Uploading ${sdk_name} SDK"
+    local latest_path="loginid-android/${sdk_name}/latest/loginid-${sdk_name}-sdk.aar"
+    local versioned_path="loginid-android/${sdk_name}/${VERSION}/loginid-${sdk_name}-sdk.aar"
+
+    echo "Uploading $sdk_path to s3://$S3_BUCKET/$latest_path"
+    aws s3 cp "$sdk_path" "s3://$S3_BUCKET/$latest_path"
+
+    echo "Uploading $sdk_path to s3://$S3_BUCKET/$versioned_path"
+    aws s3 cp "$sdk_path" "s3://$S3_BUCKET/$versioned_path"
+}
+
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $0 <sdk...>"
+    echo "Example: $0 auth mfa"
     exit 1
 fi
 
-LATEST_PATH="loginid-android/$MODULE/latest/$MODULE-release.aar"
-S3_VERSIONED_PATH="loginid-android/$MODULE/$VERSION/$MODULE-release.aar"
-
-echo "Uploading $FILE_PATH to s3://$S3_BUCKET/$LATEST_PATH"
-aws s3 cp "$FILE_PATH" "s3://$S3_BUCKET/$LATEST_PATH"
-
-echo "Uploading $FILE_PATH to s3://$S3_BUCKET/$S3_VERSIONED_PATH"
-aws s3 cp "$FILE_PATH" "s3://$S3_BUCKET/$S3_VERSIONED_PATH"
+for sdk in "$@"; do
+    upload_sdk "$sdk"
+done
